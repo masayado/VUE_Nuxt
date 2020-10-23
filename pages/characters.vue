4585f876431f2cd1d04d5e0a4af69b63fb1dcb24
masayado <masayado@uc.cl>
2020-10-22 23:04:17 -0300

    agregue grid con personajes

---
+++ pages/characters.vue
@@ -0,0 +1,22 @@
+<template>
+    <h1> Personajes</h1>
+</template>
+
+<script>
+export default {
+    name: 'component-name',
+    // props: {},
+    data: function(){
+        return {}
+    },
+    // computed: {},
+    methods: {
+        // -- Metodos
+    },
+    //components: {},
+}
+</script>
+
+<style scoped>
+    
+</style>
